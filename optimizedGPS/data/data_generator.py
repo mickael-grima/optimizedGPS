24da2b2d5d2a52a3077a0dca4c3adc5d2a726c5d
Mickael <mickaelgrima974@gmail.fr>
2017-03-09 15:12:39 +0100

    data folder documented

--- optimizedGPS/data/data_generator.py
+++ optimizedGPS/data/data_generator.py
@@ -9,11 +9,17 @@ from optimizedGPS.structure import GPSGraph
 from optimizedGPS.structure import GraphMLParser
 
 
-def generate_grid_data(length=5, width=5, **kwards):
-    """ build a grid where the top-links node has name node_0_0
-        from a given node, the reachable nodes are the one to the right and at the bottom
+def generate_grid_data(length=5, width=5, **kwargs):
     """
-    graph = GPSGraph(name=kwards.get('graph_name') or 'graph-%s-%s' % (length, width))
+    build a grid where the top-links node has name node_0_0
+    from a given node, the reachable nodes are the one to the right and at the bottom
+
+    :param length: number of nodes + 1 from links to right
+    :param width: number of nodes + 1 from top to bottom
+    :param kwargs: further options
+    :return: An GPSGraph instance
+    """
+    graph = GPSGraph(name=kwargs.get('graph_name') or 'graph-%s-%s' % (length, width))
     for i in range(length):
         for j in range(width):
             source = 'n_%s_%s' % (i, j)
@@ -34,13 +40,34 @@ def generate_grid_data(length=5, width=5, **kwards):
     return graph
 
 
-def generate_graph_from_file(file_loc, **kwards):
+def generate_graph_from_file(file_loc, **kwargs):
+    """
+    Use the GraphMLParser to extract the graph from the given file
+
+    :param file_loc: path to the file
+    :param kwargs: further options
+    :return: a GPSGraph instance
+    """
     if file_loc.endswith('.graphml'):
-        return GraphMLParser().parse(file_loc, **kwards)
+        return GraphMLParser().parse(file_loc, **kwargs)
     return None
 
 
 def generate_random_drivers(graph, total_drivers=10, av_drivers=3, seed=None):
+    """
+    Given a graph, we add total_drivers drivers as following:
+      - we take uniformly at random a source node and a reachable target node from source node
+      - we generate a random number of drivers taking using a gaussian center on av_drivers with 1 as variance
+      - we add these generated drivers to the graph, until we reach total_drivers
+      - If we get too many drivers, among the last added drivers, we remove some of them to get exactly the numberw
+        we want
+
+    :param graph: A GPSGraph instance
+    :param total_drivers: int
+    :param av_drivers: float
+    :param seed: see random package
+    :return: None
+    """
     random.seed(seed)
     total = total_drivers
     while total > 0:
@@ -55,8 +82,13 @@ def generate_random_drivers(graph, total_drivers=10, av_drivers=3, seed=None):
 
 
 def generate_test_graph(length=2, width=3):
-    """ Generate a grid graph with the given arguments
-        give always the same drivers from nodes 'n_0_0', 'n_1_0' and 'n_0_1' to final node
+    """
+    Generate a grid graph with the given arguments
+    Generate always the same drivers from nodes 'n_0_0', 'n_1_0' and 'n_0_1' to final node
+
+    :param length: see generate_grid_data
+    :param width: see generate_grid_data
+    :return: a GPSGraph instance
     """
     graph = generate_grid_data(length=length, width=width, graph_name='graph-%s-%s-test' % (length, width))
 
@@ -72,6 +104,15 @@ def generate_test_graph(length=2, width=3):
 
 
 def generate_grid_graph_random_driver(length=2, width=3, nb_drivers=10):
+    """
+    Generate a grid graph with the given length and width
+    Generate random drivers using generate_random_drivers function
+
+    :param length: see generate_grid_data
+    :param width: see generate_grid_data
+    :param nb_drivers: see generate_random_drivers. Coresponds to total_drivers argument
+    :return:
+    """
     name = 'grid-graph-%s-%s-%s' % (length, width, nb_drivers)
     graph = generate_grid_data(length=length, width=width, graph_name=name)
     generate_random_drivers(graph, total_drivers=nb_drivers)
